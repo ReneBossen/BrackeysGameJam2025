bb2a40843caf5cc6b4a726c90e1c1fc81a45ebec
ReneBossen <renebossen99@gmail.com>
2025-02-16 16:38:07 +0100

    Added door to winning room

---
+++ Assets/Scripts/Props/Door.cs
@@ -0,0 +1,10 @@
+using Brackeys.Interfaces;
+using UnityEngine;
+
+public class Door : MonoBehaviour, IActivateable
+{
+    public void OnActivate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
