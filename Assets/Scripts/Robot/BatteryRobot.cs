7335e30d7d6b29942803b55c4bbaf0729f5b6719
Xwilarg <xwilarg@protonmail.com>
2025-02-20 17:41:21 +0100

    Add battery robot

---
+++ Assets/Scripts/Robot/BatteryRobot.cs
@@ -0,0 +1,27 @@
+using Brackeys.Manager;
+
+namespace Brakeys.Robot
+{
+    public class BatteryRobot : ASpeakableRobot
+    {
+        public override string GetTranslationKey()
+        {
+            var input = ResourceManager.Instance.PlayerInput;
+
+            if (input.CurrentWeapon == null)
+            {
+                return "speak_battery_noWeapon";
+            }
+            if (input.CurrentWeapon.BaseInfo.RequiresExternalAmmosCount == 0)
+            {
+                return "speak_battery_noGoodWeapon";
+            }
+            if (!input.CurrentWeapon.NeedAmmo())
+            {
+                return "speak_battery_alreadyFull";
+            }
+            while (input.CurrentWeapon.NeedAmmo()) input.CurrentWeapon.AddAmmo();
+            return "speak_battery_ok";
+        }
+    }
+}
