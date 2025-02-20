b348de9b0d74791df15dfa9247c5db6fee628901
Xwilarg <xwilarg@protonmail.com>
2025-02-20 17:28:59 +0100

    Add robot abstract class

--- Assets/Scripts/Robot/CircusRobot.cs
+++ Assets/Scripts/Robot/CircusRobot.cs
@@ -1,40 +1,14 @@
-using Brackeys.Player;
-using Brackeys.Player.Interaction;
-using Brackeys.Translation;
-using Brackeys.VN;
 using UnityEngine;
 
 namespace Brakeys.Robot
 {
-    public class CiscusRobot : MonoBehaviour, IInteractable
+    public class CiscusRobot : ASpeakableRobot
     {
         [SerializeField]
-        private TextDisplay _display;
-
-        [SerializeField]
         private string _targetTranslationKey;
-
-        private Animator _anim;
-
-        public bool CanInteract => true;
-
-        public GameObject GameObject => gameObject;
-
-        public void Interact(PlayerController pc)
+        public override string GetTranslationKey()
         {
-            _display.ToDisplay = Translate.Instance.Tr(_targetTranslationKey);
-            _anim.SetBool("IsSpeaking", true);
-        }
-
-        private void Awake()
-        {
-            _display.ToDisplay = null;
-
-            _anim = GetComponent<Animator>();
-            _display.OnDone.AddListener(() =>
-            {
-                _anim.SetBool("IsSpeaking", false);
-            });
+            return _targetTranslationKey;
         }
     }
 }
