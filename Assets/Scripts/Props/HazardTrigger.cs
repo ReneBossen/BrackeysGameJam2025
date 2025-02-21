d91a9ed1bf802f8caecc10401cd82e1a97c7d9cb
ReneBossen <renebossen99@gmail.com>
2025-02-21 21:24:50 +0100

    Added hazard for basketball

--- Assets/Scripts/Props/HazardTrigger.cs
+++ Assets/Scripts/Props/HazardTrigger.cs
@@ -1,16 +1,23 @@
+using Brackeys.Interfaces;
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Brackeys.Props
 {
     public class HazardTrigger : MonoBehaviour
     {
-        [SerializeField] private GameObject[] _hazards;
+        [SerializeField] private List<Hazard> _hazards;
+        [SerializeField] private string _targetLayer = "Projectile";
+
         private void OnTriggerEnter(Collider other)
         {
-            if (other.CompareTag("TriggerHazard"))
+            if (other.gameObject.layer != LayerMask.NameToLayer(_targetLayer))
             {
-
+                return;
             }
+
+            _hazards.ForEach(hazard => hazard.OnActivate());
         }
     }
 }
