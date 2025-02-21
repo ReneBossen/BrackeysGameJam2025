7d64d5bd21cc24dcf7815b00e9bf47502dea958a
ReneBossen <renebossen99@gmail.com>
2025-02-21 10:42:56 +0100

    Added strengthGame animations.

--- Assets/Scripts/Props/StrengthGame.cs
+++ Assets/Scripts/Props/StrengthGame.cs
@@ -6,6 +6,19 @@ namespace Brackeys.Props
     public class StrengthGame : MonoBehaviour
     {
         private bool _isReady = true;
+        private Animator _animator;
+        private string
+            _mark1Trigger = "1mark",
+            _mark2Trigger = "2mark",
+            _mark3Trigger = "3mark",
+            _mark4Trigger = "4mark",
+            _mark5Trigger = "5mark",
+            _bellTrigger = "bell";
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
 
         public bool GetIsReady() => _isReady;
 
@@ -18,8 +31,6 @@ namespace Brackeys.Props
 
             CharacterController controller = other.controller;
 
-            Debug.Log($"CharacterController: {controller}");
-
             if (controller == null)
             {
                 return;
@@ -27,21 +38,48 @@ namespace Brackeys.Props
 
             switch (velocity.magnitude)
             {
-                case < 2:
+                case < 8:
                     _isReady = false;
+                    _animator.SetTrigger(_mark1Trigger);
                     StartCoroutine(StartCooldown());
                     break;
-                case > 2:
-                    Debug.Log($"{other} - Velocity: {velocity.magnitude}");
+                case < 15:
                     _isReady = false;
+                    _animator.SetTrigger(_mark2Trigger);
+                    StartCoroutine(StartCooldown());
+                    break;
+                case < 20:
+                    _isReady = false;
+                    _animator.SetTrigger(_mark3Trigger);
+                    StartCoroutine(StartCooldown());
+                    break;
+                case < 25:
+                    _isReady = false;
+                    _animator.SetTrigger(_mark4Trigger);
+                    StartCoroutine(StartCooldown());
+                    break;
+                case < 32:
+                    _isReady = false;
+                    _animator.SetTrigger(_mark5Trigger);
+                    StartCoroutine(StartCooldown());
+                    break;
+                case >= 32:
+                    _isReady = false;
+                    _animator.SetTrigger(_bellTrigger);
                     StartCoroutine(StartCooldown());
                     break;
             }
         }
 
+        private void PlayBell()
+        {
+            // Play bell sound
+            Debug.Log("Bell sound played");
+        }
+
         private IEnumerator StartCooldown()
         {
-            yield return new WaitForSeconds(1);
+            yield return new WaitForSeconds(2);
             _isReady = true;
         }
     }
